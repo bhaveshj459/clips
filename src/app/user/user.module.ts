9171c5aa50885c05448fc115b5bda640cb78791e
bhavesh_aloha <bjoshi@awarenesstech.com>
2023-04-03 13:43:31 +0530

    Prashant

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthModelComponent } from './auth-model/auth-model.component';
+
+@NgModule({
+  declarations: [AuthModelComponent],
+  imports: [CommonModule],
+  exports: [AuthModelComponent],
+})
+export class UserModule {}
